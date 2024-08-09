9242f71a1395cdcbf4ddf64c3c8bd4b1e259ae07
SuperSaiyan99-SS99 <supersaiyan99.ss99@gmail.com>
2024-08-09 14:48:16 -0500

    Update copyright

--- app/src/main/java/com/watabou/unbreakablepixeldungeon/items/scrolls/ScrollOfRecharging.java
+++ app/src/main/java/com/watabou/unbreakablepixeldungeon/items/scrolls/ScrollOfRecharging.java
@@ -2,6 +2,9 @@
  * Pixel Dungeon
  * Copyright (C) 2012-2015 Oleg Dolya
  *
+ * Unbreakable Pixel Dungeon
+ * Copyright (C) 2018-2024 SuperSaiyan99
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
